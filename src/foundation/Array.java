f563a4b8e31023bff8b9bb2d5348011ead9a5235
HOYEONKIM <khy18890@gmail.com>
2022-07-15 23:51:16 +0900

    배열기초
    Merge branch 'main' of https://github.com/khy11/JavaStudy.git into main

---
+++ src/foundation/Array.java
@@ -0,0 +1,54 @@
+package foundation;
+
+import java.util.ArrayList;
+
+public class Array {
+
+	public static void main(String[] args) {
+		//생성자 선언과 동시에 배열 삽입 (데이터)
+		//int[] score = new int[5] 5개의 integer를 담고있는 배열5개의 integer를 담고있는 배열
+		
+		//배열값 한번에 삽입 가능 
+		int[] score = new int[] {10,20,30,40,50}; 
+		
+		//배열 식 축약
+		int[] newScore= {10,20,30,40,50}; 
+		
+		//score라는 생성자의 길이를 세는 함수 
+		int count = score.length; 
+		
+		System.out.println(count);
+		
+		//배열에 값 참조해서 담기 
+		/*
+		 * score[0] = -1; // -1으로 초기화
+		 */		System.out.println(score[0]);
+		 
+		 
+		 //스트링배열
+		 String[] names = {"홍길동","이순신","유관순"};
+		 
+		 //리스트활용하기
+		 ArrayList<Integer> scoreList = new ArrayList<>();
+		 scoreList.add(10);
+		 scoreList.add(20);
+		 scoreList.add(30);
+		 scoreList.add(40);
+		 scoreList.add(50);
+		 
+		 System.out.println(scoreList.get(1)); //20
+		 
+		 //리스트에 인덱스값 추가하기 
+		 scoreList.add(1,100); //1번째 인덱스에 100이라는 value 넣기 
+		 
+		 System.out.println(scoreList); // [10, 100, 20, 30, 40, 50]
+		 
+		 //리스트에 인덱스값 제거하기
+		 scoreList.remove(0); // 0번째 인덱스값 삭제하기 
+		 
+		 System.out.println(scoreList); // [100, 20, 30, 40, 50]
+		 System.out.println(scoreList.get(1)); // 20
+		 System.out.println(scoreList); // [100, 20, 30, 40, 50]
+		
+	}
+}
